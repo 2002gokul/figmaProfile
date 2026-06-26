24f8ba60091ee5cffd89af10451e56c409f13c6d
2002gokul <2002gokulv@gmail.com>
2026-06-26 18:32:13 +0530

    added sidebar collision

--- figmaprofile/src/components/Navbar.js
+++ figmaprofile/src/components/Navbar.js
@@ -3,11 +3,11 @@ import companylogo from "../assets/companylogo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md px-6 py-5 flex items-center justify-between w-[100]">
+    <nav className="bg-white shadow-md px-6 py-5 flex items-center justify-between w-full ">
       <div className="flex items-center px-10">
         <img src={companylogo} alt="company logo" className="w-[116px] h-[50px] " />
       </div>
-      <ul className="flex space-x-10 text-gray-700 font-medium px-2 ">
+      <ul className="flex gap-10 text-gray-700 font-medium px-2 ">
         <li className="hover:text-blue-500 transition duration-300  cursor-pointer">Orders</li>
         <li>|</li>
         <li className="hover:text-blue-500 cursor-pointer">Holdings</li>
